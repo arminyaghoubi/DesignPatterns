3e554bf3d380ba71f58283b328219a19198ce8f9
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-12-04 00:53:33 +0330

    Has been added singleton.

---
+++ Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿using Singleton.Services;
+
+Logger logger1 = Logger.GetInstance("C:\\Logs\\ApplicationLogs.txt");
+Logger logger2 = Logger.GetInstance("C:\\Logs\\ApplicationLogs.txt");
+
+logger1.Log("Application has started from logger1...");
+logger2.Log("Application has started from logger2...");
+
+Console.ReadKey();
---
+++ Singleton/Services/Logger.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton.Services;
+
+internal sealed class Logger
+{
+    private static Logger _logger;
+    private static string? _filePath;
+
+    private Logger() { }
+
+    public static Logger GetInstance(string? filePath)
+    {
+        _filePath = filePath;
+
+        if(_logger is null)
+        {
+            _logger = new Logger();
+        }
+
+        return _logger;
+    }
+
+    public void Log(string messaeg)
+    {
+        File.AppendAllText(_filePath, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {messaeg}");
+    }
+}
