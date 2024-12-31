b4e0ca03192c5e3f4bc0cf81919ccb7bb2e69a30
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2025-01-01 00:16:37 +0330

    Prototype with shallow copy.

---
+++ Prototype/Entities/Document.cs
@@ -0,0 +1,23 @@
+﻿namespace Prototype.Entities;
+
+internal class Document: ICloneable
+{
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public string? Author { get; set; }
+
+    public Document(string? title,
+        string? content,
+        string? author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public object Clone()
+    {
+        // Shallow copy
+        return MemberwiseClone();
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,14 @@
+﻿using Prototype.Entities;
+
+Document document = new("Prototype", "Creational Design Pattern", "ArminYaghoubi");
+
+
+var document1= (Document)document.Clone();
+
+var document2= (Document)document.Clone();
+
+Console.WriteLine("-------Document1");
+Console.WriteLine($"{document1.Title}({document1.Author})\n{document1.Content}");
+
+Console.WriteLine("-------Document2");
+Console.WriteLine($"{document2.Title}({document2.Author})\n{document2.Content}");
